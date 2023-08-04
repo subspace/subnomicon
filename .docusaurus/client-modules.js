ddac5ed09db21f2c1ec280dd4a85c98fc3b82abc
Dariia Porechna <35382599+dariolina@users.noreply.github.com>
2023-08-04 10:35:18 +0200

    remove docusaurus build files

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/dariiap/Documents/GitHub/subspace/subnomicon/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/dariiap/Documents/GitHub/subspace/subnomicon/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/dariiap/Documents/GitHub/subspace/subnomicon/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/dariiap/Documents/GitHub/subspace/subnomicon/src/css/custom.css'),
-  require('/Users/dariiap/Documents/GitHub/subspace/subnomicon/node_modules/@docusaurus/plugin-google-gtag/lib/gtag'),
-];
